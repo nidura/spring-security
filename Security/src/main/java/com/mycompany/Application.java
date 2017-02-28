4aeaa22b8c397c935c33d51f099673bd3855ed63
nidura <niduraprageeth@gmail.com>
2017-02-28 16:22:30 +0530

    main class edit

--- Security/src/main/java/com/mycompany/Application.java
+++ Security/src/main/java/com/mycompany/Application.java
@@ -7,14 +7,13 @@ package com.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @ComponentScan(basePackages = "com.mycompany")
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer  {
+public class Application{
 
 
     public static void main(String[] args) {
